485e2bd50b7252680774d3a2f6bf2ac2d58b708e
Maxi <maximilianonperalta89@gmail.com>
2022-04-28 16:53:06 +0200

    Entregable 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar1 from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar1 />
     </div>
-  );
+  )
 }
+/*function App() {
+  <Navbar />
+      const Array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,];
+      const Array2 = [...Array1, 12, 13, 14];
+      console.log(Array2)
+      const Array3 = [
+        {
+            id: 1,
+            nombre: "Carmen",  
+         },
+         {
+           id: 2,
+           nombre: "Lola"
+         }
+      ]
+      const {nombre} = Array3[1]
+      console.log(nombre)
+      return (
+        <div className='app'>
+          <h1> Imprimo arrays </h1>
+          <div>Hola {nombre}</div>
+          {Array3.map((persona) => (
+            <p>{persona.nombre}</p>
+          ))}
+        </div>
+      )
+    }*/
 
 export default App;
+
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import {Navbar, Container, Nav, NavDropdown, Form, FormControl, Button,} from "react-bootstrap"
+export default function Navbar1(){
+    return (
+        <header className="headerNav">
+        <Navbar variant="dark" expand="lg">
+          <Container>
+    <Navbar.Brand href="#home">
+      <img
+        src="./cordillera.png"
+        width="60"
+        height="60"
+        className="d-inline-block align-top"
+        alt=""
+      />
+    </Navbar.Brand>
+    <Container className="tituloHeader">
+          <Navbar.Brand href="#home">Nombre de Marca </Navbar.Brand>
+  </Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+        <FormControl
+          type="email"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-light">Search</Button>
+      </Form>
+          </Navbar.Collapse>
+          
+        </Container>
+      </Navbar>
+      </header>
+
+        
+        /*<header className="header">
+        <div className="logo">Logo</div>
+        <ul></ul>
+            
+            
+        </header>*/
+    );
+}
